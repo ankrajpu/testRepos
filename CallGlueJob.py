66c26ced6d4688b137e5235be80be9143a920dee
ankrajpu <90900189+ankrajpu@users.noreply.github.com>
2021-09-18 18:11:36 -0700

    Updated Comments

--- CallGlueJob.py
+++ CallGlueJob.py
@@ -9,7 +9,14 @@ s3 = boto3.client('s3')
 glue = boto3.client(service_name='glue')
 glueJobName = 'ProcessData'
 
+
 def lambda_handler(event, context):
+    """
+    :param event:
+    :param context:
+    :return: Exception
+    Lambda function to trigger the Glue Job in case of Upload Event in S3
+    """
     try:
         # glue.start_crawler(Name='ProcessData')
         glue.start_job_run(JobName = glueJobName)
